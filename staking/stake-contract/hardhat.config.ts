32304c85ad151f6a341fa5c010abe4bcf9b00080
wendy <1343177851@qq.com>
2025-11-27 09:15:48 +0800

    feat(staking/stake-contract): init hardhat config

--- staking/stake-contract/hardhat.config.ts
+++ staking/stake-contract/hardhat.config.ts
@@ -1,5 +1,15 @@
+/*
+ * @Author: zhangjian
+ * @Date: 2025-11-26 09:02:00
+ * @LastEditTime: 2025-11-26 11:32:14
+ * @LastEditors: zhangjian
+ * @Description: hardhat配置
+ */
 import hardhatToolboxViemPlugin from "@nomicfoundation/hardhat-toolbox-viem";
 import { configVariable, defineConfig } from "hardhat/config";
+import "dotenv/config";
+
+const sepoliaAccounts = process.env.SEPOLIA_PRIVATE_KEY ? [process.env.SEPOLIA_PRIVATE_KEY] : [];
 
 export default defineConfig({
   plugins: [hardhatToolboxViemPlugin],
@@ -25,6 +35,14 @@ export default defineConfig({
     },
   },
   networks: {
+    localhost: {
+      url: "http://127.0.0.1:8545",
+      chainId: 31337,  // Hardhat 本地网络的默认 chainId
+      type: "http",     // 声明这是一个 HTTP 网络
+      accounts: {
+        mnemonic: "test test test test test test test test test test test junk",
+      },
+    },
     hardhatMainnet: {
       type: "edr-simulated",
       chainType: "l1",
@@ -33,11 +51,17 @@ export default defineConfig({
       type: "edr-simulated",
       chainType: "op",
     },
+    // sepolia: {
+    //   type: "http",
+    //   chainType: "l1",
+    //   url: configVariable("SEPOLIA_RPC_URL"),
+    //   accounts: [configVariable("SEPOLIA_PRIVATE_KEY")],
+    // },
     sepolia: {
       type: "http",
-      chainType: "l1",
-      url: configVariable("SEPOLIA_RPC_URL"),
-      accounts: [configVariable("SEPOLIA_PRIVATE_KEY")],
+      chainId: 11155111,
+      url: process.env.SEPOLIA_RPC_URL || "",
+      accounts: sepoliaAccounts,
     },
   },
 });
